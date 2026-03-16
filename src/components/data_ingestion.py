767c30555c20607190c57f9192dfb4ceb685109d
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-03-16 11:00:52 +0530

    fix: split data into train and test, rename vairables of config

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 from src.entity.data_ingestion_entity import DataIngestionEntityConfig, DataIngestionArtifactEntity
 
 class DataIngestion:
@@ -8,23 +10,24 @@ class DataIngestion:
         self.data_ingestion_entity_config = data_ingestion_entity_config
     
     def initiate_data_ingestion(self) -> DataIngestionArtifactEntity:
-        data_ingested_dir = os.path.dirname(self.data_ingestion_entity_config.ingested_dir_train_file_path)
+        data_ingested_dir = os.path.dirname(self.data_ingestion_entity_config.ingestion_dir_train_file_path)
         os.makedirs(data_ingested_dir, exist_ok=True)
 
         features_df = pd.read_csv('/Users/uday/Desktop/walmart-sales-prediction/data/features.csv')
         stores_df = pd.read_csv('/Users/uday/Desktop/walmart-sales-prediction/data/stores.csv')
-        train_df = pd.read_csv('/Users/uday/Desktop/walmart-sales-prediction/data/train.csv')
-        test_df = pd.read_csv('/Users/uday/Desktop/walmart-sales-prediction/data/test.csv')
+        data_df = pd.read_csv('/Users/uday/Desktop/walmart-sales-prediction/data/data.csv')
 
-        merged_train_df = train_df.merge(stores_df, how='left').merge(features_df, how='left')
-        merged_test_df = test_df.merge(stores_df, how='left').merge(features_df, how='left')
+        merged_data_df = data_df.merge(stores_df, how='left').merge(features_df, how='left')
+        train, test = train_test_split(merged_data_df, test_size=0.2, random_state=42)
 
-        merged_train_df.to_csv(self.data_ingestion_entity_config.ingested_dir_train_file_path, index=False, header=True)
-        merged_test_df.to_csv(self.data_ingestion_entity_config.ingested_dir_test_file_path, index=False, header=True)
+
+        train.to_csv(self.data_ingestion_entity_config.ingestion_dir_train_file_path, index=False, header=True)
+        test.to_csv(self.data_ingestion_entity_config.ingestion_dir_test_file_path, index=False, header=True)
+        merged_data_df.to_csv(self.data_ingestion_entity_config.ingestion_dir_data_file_path, index=False, header=True)
 
         return DataIngestionArtifactEntity(
-            train_file_path=self.data_ingestion_entity_config.ingested_dir_train_file_path,
-            test_file_path=self.data_ingestion_entity_config.ingested_dir_test_file_path
+            train_file_path=self.data_ingestion_entity_config.ingestion_dir_train_file_path,
+            test_file_path=self.data_ingestion_entity_config.ingestion_dir_test_file_path
         )
 
 
--- src/entity/data_ingestion_entity.py
+++ src/entity/data_ingestion_entity.py
@@ -1,13 +1,14 @@
 import os
 
 from src.entity import TrainingPipelineConfig
-from src.constants.constants import DATA_INGESTION_DIR, INGESTION_TRAIN_FILE_NAME, INGESTION_TEST_FILE_NAME
+from src.constants.constants import DATA_INGESTION_DIR, INGESTION_TRAIN_FILE_NAME, INGESTION_TEST_FILE_NAME, INGESTION_DATA_FILE_NAME
 
 class DataIngestionEntityConfig:
     def __init__(self, training_pipeline_config: TrainingPipelineConfig):
         self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR)
-        self.ingested_dir_train_file_path = os.path.join(self.data_ingestion_dir, INGESTION_TRAIN_FILE_NAME)
-        self.ingested_dir_test_file_path = os.path.join(self.data_ingestion_dir, INGESTION_TEST_FILE_NAME)
+        self.ingestion_dir_train_file_path = os.path.join(self.data_ingestion_dir, INGESTION_TRAIN_FILE_NAME)
+        self.ingestion_dir_test_file_path = os.path.join(self.data_ingestion_dir, INGESTION_TEST_FILE_NAME)
+        self.ingestion_dir_data_file_path = os.path.join(self.data_ingestion_dir, INGESTION_DATA_FILE_NAME)
 
 class DataIngestionArtifactEntity:
     def __init__(self, train_file_path: str, test_file_path: str):
