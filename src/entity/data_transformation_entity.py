44c2fc73f208768c4c5690125f99acacbe273a12
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-03-17 10:54:09 +0530

    task: file name in constants

--- src/entity/data_transformation_entity.py
+++ src/entity/data_transformation_entity.py
@@ -2,12 +2,14 @@ import os
 
 from src.entity import TrainingPipelineConfig
 
+from src.constants.constants import DATA_TRANSFORMATION_DIR, TRANSFORMED_DATA_DIR, TRANSFORMED_OBJECT_DIR, TRANSFORMED_TRAIN_FILE_NAME, TRANSFORMED_TEST_FILE_NAME, TRANSFORMED_OBJECT_FILE_NAME
+
 class DataTransformationEntityConfig:
     def __init__(self, training_pipeline_config: TrainingPipelineConfig):
-        self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_transformation")
-        self.data_transformation_train_file_path = os.path.join(self.data_transformation_dir, "transformed_data", "train.npy")
-        self.data_transformation_test_file_path = os.path.join(self.data_transformation_dir, "transformed_data", "test.npy")
-        self.data_transformation_object_file_path = os.path.join(self.data_transformation_dir, "transformated_object", "preprocessor.pkl")
+        self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir, DATA_TRANSFORMATION_DIR)
+        self.data_transformation_train_file_path = os.path.join(self.data_transformation_dir, TRANSFORMED_DATA_DIR, TRANSFORMED_TRAIN_FILE_NAME)
+        self.data_transformation_test_file_path = os.path.join(self.data_transformation_dir, TRANSFORMED_DATA_DIR, TRANSFORMED_TEST_FILE_NAME)
+        self.data_transformation_object_file_path = os.path.join(self.data_transformation_dir, TRANSFORMED_OBJECT_DIR, TRANSFORMED_OBJECT_FILE_NAME)
 
 class DataTransformationArtifactEntity:
     def __init__(self, data_transformation_train_file_path: str, data_transformation_test_file_path: str, data_transformation_object_file_path: str):
